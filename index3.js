7a667f467a44a5f9d1d5ffe00db224f420fbc451
Mr. X <mr.x@Mrs-MacBook-Air.local>
2022-08-02 16:51:26 +0600

    added push-pop and comparisons

--- index3.js
+++ index3.js
@@ -3,7 +3,7 @@
 // var totalPrice = price + price2;
 // console.log(price+price2);
 
-
+// moduole 17.2
 // var price = 100; 
 // console.log(typeof price);
 
@@ -12,7 +12,29 @@
 // var romantic;
 // console.log(typeof romantic);
 
+//module 17-3
+// var friendsAge =[11, 21, 45, 17, 14, 12, 39];
 
-var friendsAge =[11, 21, 45, 17, 14, 12, 39];
+// var picnicFee =[5000, 2000, 3000, 1500, 7000, 6000];
+// var players = ['ronaldo', 'messi', 'lewandowski','de gea','osaka'];
+// console.log(players);
+// console.log(picnicFee.length);
+// console.log(friendsAge);
+
+
+// module 17-4
+var numbers = [45, 68, 76, 98, 87];
+// get elements by index
+console.log(numbers[0]);
+//get element value by index
+var element = numbers[1];
+console.log(element);
+//set element vlaue
+numbers[1]= 77;
+console.log(numbers);
+//find index if an element
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
+
 
 
---
+++ push-pop.js
@@ -0,0 +1,24 @@
+// var numbers =[ 78, 45, 98, 46];
+// console.log(numbers);
+// numbers.push(633, 54 );
+// console.log(numbers);
+// //use push to add elements to the parts of the array 
+// var friends = [ 'balam','kalam','salam'];
+// console.log(friends);
+// friends.push('golam');
+// console.log(friends);
+// console.log(typeof friends[0]);
+
+// //use pop to remove the last element out of the array
+
+// numbers.pop();
+// numbers.pop();
+// console.log(numbers);
+
+// console.log(element);
+
+
+//comparison
+console.log(5 < 6);
+///
+
