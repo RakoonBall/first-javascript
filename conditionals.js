cea33303fe5d368612bcf68c657c2fdcaf9a2578
Mr. X <mr.x@Mrs-MacBook-Air.local>
2022-08-02 17:43:14 +0600

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,43 @@
+var iphonePrice = 79000;
+var myBudget =9500;
+var cars = 0;
+
+//if iphone prize is less than my budget i will buy the iphone 
+if(iphonePrice<myBudget){
+    console.log("i will buy the iphone");
+}else{
+    console.log("i won't buy the iphone");
+}
+var chickenPrice = 500;
+var myMoney= 50;
+if(chickenPrice <= myMoney){
+    console.log("i will buy chicken");
+}else{
+
+}
+
+
+//7.7 module
+//multiple conditions
+var salary = 50000;
+var isGraduated = true;
+if(isGraduated === true && salary > 50000 && cars >=1){
+    console.log("cholo jai bohudur");
+}else{
+    console.log("tor kopale kichu nai");
+}
+
+//7.8 module
+var money = 28 ;
+var danishPrice = 45;
+var butterPrice = 30 ;
+var toastPrice = 20;
+if(danishPrice < money){
+    console.log("dan dan danish khamu");
+}else if(butterPrice<money){
+    console.log('butter bon khamu');
+}else if(toastPrice <money){
+    console.log("toast khamu");
+}else{
+    console.log("khali chai shasther jonno valo");
+}
