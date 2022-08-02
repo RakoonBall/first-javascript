1c6a5afd00fe71dc6beb11eec3bf527631c7d93a
Mr. X <mr.x@Mrs-MacBook-Air.local>
2022-08-02 15:08:03 +0600

    added

--- index3.js
+++ index3.js
@@ -4,13 +4,15 @@
 // console.log(price+price2);
 
 
-var price = 100; 
-console.log(typeof price);
+// var price = 100; 
+// console.log(typeof price);
 
-var isHappy=true;
-console.log(typeof gisHappy);
-var romantic;
-console.log(typeof romantic)
+// var isHappy=true;
+// console.log(typeof gisHappy);
+// var romantic;
+// console.log(typeof romantic);
 
+
+var friendsAge =[11, 21, 45, 17, 14, 12, 39];
 
 
